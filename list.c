e762dd3edab71bfdf6c73e768955c7c6b6db0fee
Michał Walczak <mjf.walczak@gmail.com>
2013-08-11 00:41:58 +0200

    Added list_insert() and list_pluck()
    Extended the lib with two list utility functions.
    list_insert() adds a new node with given data after the provided node.
    list_pluck() removes any given node and returns it's data.

--- list.c
+++ list.c
@@ -143,3 +143,60 @@ void destroy_list(list_p list){
 	free(list);
 }
 
+void list_insert(list_p list, lnode_p before, void *data, int size){
+    if (list->first == NULL) {
+        list_add(list, data, size);
+    } 
+    else if (before == list->last) {
+        list_add(list, data, size);
+    }
+    else if (before == NULL) {
+        lnode_p node = (lnode_p)malloc(sizeof(struct linked_node));
+        node->data = malloc(size);
+        memcpy(node->data, data, size);
+
+        node->next = list->first;
+        node->prev = NULL;
+        node->next->prev = node;
+        list->first = node;
+        list->length++;
+    }
+    else {
+        lnode_p node = (lnode_p)malloc(sizeof(struct linked_node));
+        node->data = malloc(size);
+        memcpy(node->data, data, size);
+
+        node->next = before->next;
+        node->prev = before;
+        before->next = node;
+        node->next->prev = node;
+        list->length++;
+    }
+}
+
+void* list_pluck(list_p list, lnode_p removed){
+    if(removed == NULL){
+        return NULL;
+    }
+    else if(list->first == removed && list->last == removed){
+        list->first = NULL;
+        list->last = NULL;
+    }
+    else if(list->first == removed){
+        list->first = removed->next;
+        removed->next->prev = NULL;
+    }
+    else if(list->last == removed){
+        list->last = removed->prev;
+        removed->prev->next = NULL;
+    }
+    else{
+        removed->prev->next = removed->next;
+        removed->next->prev = removed->prev;
+    }
+
+    void* data = removed->data;
+    free(removed);
+    list->length--;
+    return data;
+}
